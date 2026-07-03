873284d5f0dbd703582fade6cb7b63affc086e03
adam lamouri <adam.lamouri@epitech.eu>
2026-07-03 20:54:23 +0200

    Initial Commit

---
+++ flipflapp-ios/flipflapp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipflapp_iosApp.swift
+//  flipflapp-ios
+//
+//  Created by Adam on 03/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct flipflapp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
